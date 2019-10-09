79b1457ce2f07ae485ad9fcffccf03958030eaad
Yan Simonov <simonovyan1999@gmail.com>
2019-10-09 16:06:56 +0200

    Zmena listu citys a inputu

--- teleBot.py
+++ teleBot.py
@@ -1,12 +1,17 @@
 import telebot
 import pyowm
-import emoji
 
-bot = telebot.TeleBot("902452228:AAFxwaA2yilyGl7JppE3pwoe8545SDJ88Ns")
-owm = pyowm.OWM('7c83d4d6fc0856c7a2ed516b47ebbabd', language='ru')
+token_bot_Shevnin="902452228:AAFxwaA2yilyGl7JppE3pwoe8545SDJ88Ns"
+API_key_Shevnin='7c83d4d6fc0856c7a2ed516b47ebbabd'
+
+token_bot_Sim="971477626:AAHYok-sna2mJ-UnkEYGGLlyNhcZBZ_1h-o"
+API_key_Sim='9262eaa5e82b734436f39f3e9c42f0bd'
+
+bot = telebot.TeleBot(token_bot_Shevnin)
+owm = pyowm.OWM(API_key_Shevnin, language='ru')
 def t_9(string):
     city = string
-    with open("citys.txt", 'r') as citys:
+    with open("citys.txt", 'r', encoding='utf8') as citys:
         towns_list = []
         index_list = []
         maximum = 0
@@ -21,7 +26,6 @@ def t_9(string):
                     city1 += 1
                     city2 += 1
             index_list.append(index)
-        #print(index_list)
 
         for i in range(len(index_list)):
             if maximum < index_list[i]:
@@ -29,14 +33,16 @@ def t_9(string):
                 index = i
         towns_list = [line.rstrip() for line in towns_list]
         string_ret = towns_list[index]
+
         return string_ret
-        #print(string_ret)
 
 @bot.message_handler(content_types=['text'])
 def send_weather(message):
     #answer = "Добро пожаловать в weatherbot, напиши название города в котором ты находишься \n"
-
-    inputs = t_9(message.text)
+    inputs = message.text.lower()
+    inputs = inputs.title()
+    print(inputs)
+    inputs = t_9(inputs)
     observation = owm.weather_at_place(inputs)
     status = observation.get_weather()
     wind = status.get_wind()["speed"]
